522db5ee0b934d0e4aa22add6261670f32fe1a37
Xolane-Black <xolanemnisi@gmail.com>
2022-10-14 11:40:53 -0700

    hello world mandatory questions

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdion.h>
+
+/**
+  * main - Entry point
+  *
+  * Return: 0 Always (Sucess)
+  */
+
+int main(void)
+{
+	print("Size of a char: %i byte(s)\n", sizeof(char));
+	print("Size of a int: %i byte(s)\n", sizeof(int));
+	print("Size of a long int: %i byte(s)\n", sizeof(long int));
+	print("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+	print("Size of a float: %i byte(s)\n", sizeof(float));
+	return (0);
+}
