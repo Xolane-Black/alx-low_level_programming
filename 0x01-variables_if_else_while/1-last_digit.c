44851bce653b168fe3adae5243b0237a373a8391
Xolane-Black <xolanemnisi@gmail.com>
2022-10-14 15:35:57 -0700

    1-last digit tast

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 		printf("Last digit of %i is %i and is 0\n", n, ld);
 	else if (ld > 5)
 		printf("Last digit of %i is %i and is greater than 5\n", n, ld);
-	else if (ld > 0 && ld < 6)
+	else if (ld != 0 && ld < 6)
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, ld);
 	return (0);
 }
