97add4dec2fa3e126440f93d675caee01c084aeb
Xolane-Black <xolanemnisi@gmail.com>
2022-10-15 05:45:22 -0700

    putchar command

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: 0 always (Success)
+  */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
