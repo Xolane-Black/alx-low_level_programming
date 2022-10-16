5d59eb34e6922a5033f4a3e5cecfe3d55127f4f9
Xolane-Black <xolanemnisi@gmail.com>
2022-10-16 08:55:21 -0700

    task 7, alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: 0 always (Success)
+  */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a--)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
